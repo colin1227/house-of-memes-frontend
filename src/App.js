c524032bc7d9c07dbbc5d507f9b2e458b45690d4
Colin Daniel <colin.daniel1227@gmail.com>
2021-01-14 00:40:48 -0600

    cool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// const axios = require("axios");
+// const thingy = async(e) => {
+//   try {
+
+//     const move = await axios.post("http://localhost:9000/upload", e.target.files[0], {
+//       headers: {
+//         'Content-Type': 'multipart/form-data'
+//       }
+//     })
+//     console.log(move);
+
+//   } catch (err) {
+//     console.log(err.message)
+//   }
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form
+      id='uploadForm' 
+      action='http://localhost:9000/upload' 
+      method='post' 
+      name="pogU"
+      encType="multipart/form-data">
+        <input type="file" name="sampleFile" />
+        <input type='submit' value='Upload!' />
+      </form>  
+      {/* <input type="file" name="PogU" id="fileUpload" onInput={(e) => thingy(e)} /> */}
     </div>
   );
 }
