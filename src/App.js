2cc5c1316bdbfbcd5809d214ca48ff29fd7ccc0a
Colin Daniel <colin.daniel1227@gmail.com>
2021-02-04 13:05:27 -0500

    direction change incoming

--- src/App.js
+++ src/App.js
@@ -18,24 +18,23 @@ const App = () => {
   const [ status, changeStatus] = useState('');
   const history = useHistory();
 
-
+  
   if ((username && username.length <= 0 )|| (status && 0 >= status.length)) history.push("/u/sign-in");
 
   return (
-
-    <Router>
       <div className="App">
+        <div className="fadeIn" />
+        <Router>
           <Switch>
             <Route exact path="/" component={ () => <Landing username={username} status={status} />} />
             <Route exact path="/m/" component={ () => <Viewer username={username} status={status} />} />
             <Route exact path="/m/upload" component={ () => <Upload username={username} status={status} />} />
             {/* <Route exact path="/m/category" /> */}
             <Route exact path="/u/sign-in" component={ () => <SignInForum username={username} status={status} logUsername={logUsername} changeStatus={changeStatus} />} />
-            <Route exact path="/u/sign-up" component={ () => <SignUpForum username={username} status={status} logUsername={logUsername} changeStatus={changeStatus} />} />
-            
+            <Route exact path="/u/sign-up" component={ () => <SignUpForum username={username} status={status} logUsername={logUsername} changeStatus={changeStatus} />} />   
           </Switch>
+        </Router>
       </div>
-    </Router>
   );
 }
 
