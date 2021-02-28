7ec379dfc6a7877b7b1b9b1017bde346d5f79f90
Colin Daniel <colin.daniel1227@gmail.com>
2021-02-28 14:06:50 -0500

    yes

--- src/Containers/Viewer/Viewer.js
+++ src/Containers/Viewer/Viewer.js
@@ -90,7 +90,7 @@ const Viewer = ({ username }) => {
   },[initalMeme, viewIndex.count, memeUrls])
 
   useEffect(() => {
-    if (memeUrls.length <= viewIndex.count + 1 && memeUrls.length > 0) {
+    if (memeUrls.length <= viewIndex.count + 1) {
       handleImportMemes(4);
     }
   }, [memeUrls, viewIndex.count, formatList, handleImportMemes]);
